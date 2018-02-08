e87b717eed3f1b56afbea7496dd40743c2ec5ede
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 00:40:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ attr_accessor :sender, :receiver, :amount, :status
     sender.balance -= @amount
     receiver.balance += @amount
     self.status = "complete"
-    if sender.valid? == false 
+    if sender.valid? == false
       self.status = "rejected"
     end 
   end
