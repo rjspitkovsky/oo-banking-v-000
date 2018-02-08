70adc6caf87616c7f185a44463c47cea93c95acb
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 00:41:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ attr_accessor :sender, :receiver, :amount, :status
     self.status = "complete"
     if sender.valid? == false
       self.status = "Transaction rejected. Please check your account balance."
-    end 
+    end
   end
 
 end
